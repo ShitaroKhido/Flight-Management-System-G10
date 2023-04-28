5c0368178df2918665e118e5f55af4bfd468aa8d
PV-VIBOL <vibolpv20@gmail.com>
2023-04-29 00:26:36 +0700

    [FMSG-13] : Create attributes in classes

---
+++ src/Plane/Plane.ts
@@ -0,0 +1,5 @@
+export class Ticket{
+    constructor(protected masterRefferenceNumber: number){
+        this.masterRefferenceNumber = masterRefferenceNumber;
+    }
+}
---
+++ src/Tickets/FrequentFlyer.ts
@@ -0,0 +1,6 @@
+export class FrequentFlyer {
+    constructor(private milePoint: number, private flightCount: number){
+        this.flightCount = flightCount;
+        this.milePoint = milePoint
+    }
+}
