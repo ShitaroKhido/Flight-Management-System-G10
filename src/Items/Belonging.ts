9d3d8e7973ebec1b7e45266fcabe3c2197c82e50
ShitaroKhido <khidpagna01@gmail.com>
2023-04-29 00:03:50 +0700

    [FMSG-7]: Added Item, Belonging, Meal class.

---
+++ src/Items/Belonging.ts
@@ -0,0 +1,7 @@
+import { Item } from "./Item";
+
+export class Belonging extends Item {
+  constructor(name: string, weight: number, private category: ItemCategory) {
+    super(name, weight);
+  }
+}
---
+++ src/Items/Meal.ts
@@ -0,0 +1,7 @@
+import { Item } from "./Item";
+
+export class Meal extends Item {
+  constructor(name: string, weight: number, private category: MealType) {
+    super(name, weight);
+  }
+}
