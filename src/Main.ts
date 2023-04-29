06aec6a98de0468dd3391ed8717b9f46075096c0
ShitaroKhido <khidpagna01@gmail.com>
2023-04-29 10:47:45 +0700

    [FMSG-5]: Testing and added method.

--- src/Main.ts
+++ src/Main.ts
@@ -1,1 +1,22 @@
-// Main file
+import { Airline } from "./Airport/Airline";
+import { Airport } from "./Airport/Airport";
+import { BoardingGate } from "./Airport/BoardingGate";
+
+// Declare Airport Object:
+const phnomPenhAirport = new Airport(
+  "Phnom Penh Internation Airport",
+  "Cambodia",
+  "Phnom Penh"
+);
+// Declare BoardingGate Object:
+phnomPenhAirport.assignBoardinGates(10);
+// Declare Arlines Object:
+const cambodiaAirline = new Airline("Cambodia Airline");
+const ukrainAirline = new Airline("Unkrain Airline");
+
+// Main Code:
+phnomPenhAirport.registerAirline(cambodiaAirline);
+phnomPenhAirport.registerAirline(ukrainAirline);
+
+// Ouput
+console.log(phnomPenhAirport);
