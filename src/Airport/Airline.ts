fd15e229663a86ff02ea972c3b3f46ef5c0917bc
PV-VIBOL <vibolpv20@gmail.com>
2023-04-29 08:43:52 +0700

    [FMSG-11] : Update attributes of classes

--- src/Airport/Airline.ts
+++ src/Airport/Airline.ts
@@ -1,4 +1,8 @@
+import { FlightRoute } from "../Flights/FlightSchedule";
+
 export class Airline{
+    private aviableRoutes: FlightRoute[]= [];
+    
     constructor(private name:string){
         this.name = name;
     }
--- src/Airport/Airport.ts
+++ src/Airport/Airport.ts
@@ -1,4 +1,10 @@
+import { BoardingPass } from "../Tickets/BoardingPass";
+import { Airline } from "./Airline";
+
 export class Airport{
+    private gates: BoardingPass[] = [];
+    private airlines: Airline[]= [];
+    
     constructor(private name: string, private country: string, private city: string){
         this.name = name;
         this.city = city;
